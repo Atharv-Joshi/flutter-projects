779d7cc3f82cfe96cf1f39a8a63818d3d11af96b
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-04-26 23:39:32 +0530

    category_item as template for every item in grid which is displayed on category _screens

--- meals_app/meals_app/lib/categories_screen.dart
+++ meals_app/meals_app/lib/categories_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [
-        
-      ],
+      children: DUMMY_CATEGORIES.map((e) => CategoryItem(title: e.title,color: e.color,)).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3/2,
---
+++ meals_app/meals_app/lib/category_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem({this.color, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color.withOpacity(0.7) , color],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
