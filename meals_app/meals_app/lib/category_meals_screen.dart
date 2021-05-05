76ced003f5740a67168e7eb985ee0f4b38bc9c13
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-05-05 22:38:45 +0530

    category meals screen displays titles of recepies

--- meals_app/meals_app/lib/category_meals_screen.dart
+++ meals_app/meals_app/lib/category_meals_screen.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/categorymealsscreen';
   @override
   Widget build(BuildContext context) {
     final Map data = ModalRoute.of(context).settings.arguments;
+    final categoryId = data['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(data['title']),
-      ),
-      body: Center(
-        child: Text('Category Meals  Screen'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(data['title']),
+        ),
+        body: ListView.builder(
+            itemCount: categoryMeals.length,
+            itemBuilder: (ctx, index) {
+              return (Text(categoryMeals[index].title));
+            }));
   }
 }
